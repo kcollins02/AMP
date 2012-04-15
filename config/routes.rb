c08a85e8a5c0af9bffffdcaacfb207c81b5f7c57
Kevin Collins <kevin.collins02@gmail.com>
2012-04-14 23:12:21 -0400

    added url alias for register

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Assistant::Application.routes.draw do
   get "home/index"
   root :to => "home#index"
 
+  match 'register' => 'users#new', :as => :register
   match 'login' => 'user_sessions#new', :as => :login
   match 'logout' => 'user_sessions#destroy', :as => :logout
 
